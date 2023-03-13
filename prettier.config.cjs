2768e945a35c625d62897859d77eee70e3462717
Spectrum-Elliott <elliottives17@gmail.com>
2023-03-13 02:05:30 -0500

    Initial commit

---
+++ prettier.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+module.exports = {
+    plugins: [],
+    trailingComma: 'es5',
+    tabWidth: 4,
+    semi: true,
+    singleQuote: true,
+    printWidth: 160,
+};
---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            screens: {
+                'max-s': { max: '640px' },
+            },
+        },
+    },
+    plugins: [],
+};
